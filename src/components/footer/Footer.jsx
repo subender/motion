20da319627b177f91ce4950e5c3d0ac7af0b9060
subender <subender.io@outlook.com>
2023-03-26 20:29:48 +0530

    added simple footer component.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,51 @@
 import React from "react";
 import "./Footer.scss";
+
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menu_items">
+          <li className="menu_item">Terms Of Use</li>
+          <li className="menu_item">Privacy-Policy</li>
+          <li className="menu_item">About</li>
+          <li className="menu_item">Blog</li>
+          <li className="menu_item">FAQ</li>
+        </ul>
+        <div className="info_text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </div>
+        <div className="social_icons">
+          <span className="icon">
+            <FaFacebookF />
+          </span>
+          <span className="icon">
+            <FaInstagram />
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
 };
 
 export default Footer;
