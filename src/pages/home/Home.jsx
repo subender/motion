5ee0c9a9fb2fc2fe3efec9333261461d94c08f64
subender <subender.io@outlook.com>
2023-03-26 20:33:23 +0530

    added some height for testing navbar hide function

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./style.scss";
 import HeroBanner from "./HeroBanner/HeroBanner";
+import Trending from "./trending/Trending";
+
 const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner />
+      <Trending />
+      <div style={{ height: 1000 }}></div>
     </div>
   );
 };
