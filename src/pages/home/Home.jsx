97d72a9ae7fa2004a046c1cf6e2b17e4ba3c1a31
subender <subender.io@outlook.com>
2023-03-28 23:39:24 +0530

    Included Popular and TopRated component.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import "./style.scss";
 import HeroBanner from "./HeroBanner/HeroBanner";
 import Trending from "./trending/Trending";
+import Popular from "./popular/Popular";
+import TopRated from "./topRated/TopRated";
 
 const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner />
       <Trending />
-      <div style={{ height: 1000 }}></div>
+      <Popular />
+      <TopRated />
     </div>
   );
 };
